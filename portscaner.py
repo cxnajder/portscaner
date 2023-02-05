3530913e37c85ddb662e53b5815b6ba617755a70
cxnajder <98214055+cxnajder@users.noreply.github.com>
2023-02-05 19:53:03 +0100

    Update portscaner.py

--- portscaner.py
+++ portscaner.py
@@ -1,20 +1,24 @@
-import sys
+import os
 import socket
+import subprocess
+
+prefiks = input("Prefiks: ")
+pierwszy_adr = input("Pierwszy adres: ")
+ostatni_adr = input("Ostatni adres: ")
+
+os.system(f"./znajdz_adresy.sh {prefiks} {pierwszy_adr} {ostatni_adr} > adresy.txt")
 
 plik_adresy = open("adresy.txt", "r+")
 if plik_adresy.readable():
     zbior_adresow = plik_adresy.readlines()
     for adres in zbior_adresow:
-        print(adres)
+        #print(adres)
         print("="*10)
         print("Skanowanie hosta", adres)
         for port in range (1,65535):
             soket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             soket.settimeout(0.5)
-            try:
-                wynik_polaczenia = soket.connect((adres, port))
+            wynik_polaczenia = soket.connect_ex((adres.strip(), port))
+            if not wynik_polaczenia:
                 print("Port", port, "jest otwarty")
-            except:
-                pass
-            finally:
-                soket.close()
+            soket.close()
