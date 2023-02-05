de1c21b038792e2301e54f3a28028d92f48e42a1
cxnajder <98214055+cxnajder@users.noreply.github.com>
2023-02-05 19:52:27 +0100

    Update znajdz_adresy.sh

--- znajdz_adresy.sh
+++ znajdz_adresy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #ifconfig | grep -oE "([0-9]{1,3}[\.]){3}[0-9]{1,3}" | sort | uniq
-prefiks=192.168.1 #należy zmienić prefiks na sieci którą chcemy przeskanować
-pierwszy_adr=1  # ustawiamy zakres od
-ostatni_adr=254 # do
+prefiks=$1 #należy zmienić prefiks na sieci którą chcemy przeskanować
+pierwszy_adr=$2 # ustawiamy zakres od
+ostatni_adr=$3 # do
 for (( ip=$pierwszy_adr; ip<=$ostatni_adr; ip++ ))
 do
 	ping -c 1 $prefiks.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
